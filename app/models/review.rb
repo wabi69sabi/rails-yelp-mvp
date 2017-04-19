02ef88345d7f51d39a60751b76a034842313b816
Pasquale Kress <wabisabisf@gmail.com>
2017-04-19 16:20:12 +0200

    done...just need to add designs

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
 
-  # RATING = [0, 1, 2, 3, 4, 5]
+  RATING = [0, 1, 2, 3, 4, 5]
 
   belongs_to :restaurant
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,29 +7,3 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
-
-
-# Rails.application.routes.draw do
-
-#   namespace :admin do
-#     resources :restaurants, only: [:index]
-#   end
-
-#   resources :restaurants do
-
-#     collection do
-#       get 'top', to: "restaurants#top"
-#     end
-
-#     member do
-#      get 'chef', to: "restaurants#chef"
-#     end
-
-#     resources :reviews, only: [:new, :create]
-
-#   end
-
-#   resources :reviews, only: [:show, :edit]
-
-# end
